902f8f67eb966fd9bf8dfcffec1588e61f158d14
zhoupengfei <1813763637@qq.com>
2022-12-16 17:53:32 +0800

    [refactor]局部锁的模板实现

--- src/mutex.h
+++ src/mutex.h
@@ -23,8 +23,78 @@
 namespace ChiXiao{
 
 
+/**
+ * @brief 局部锁的模板实现
+ *
+*/
+template<class T>
+struct ScopedLockImpl
+{
+    private:
+        // mutex
+        T& m_mutex;
+        // 是否已上锁
+        bool m_locked;
+
+    /**
+     * @brief 构造函数，自动加锁
+     *
+     */
+    ScopedLockImpl(T& mutex)
+        :m_mutex(mutex){
+        m_mutex.lock();
+        m_locked = true;
+    }
+
+    /**
+     * @brief 析构函数,自动释放锁
+     *
+     */
+    ~ScopedLockImpl()
+    {
+        unlock();
+    }
+
+    /**
+     * @brief 加锁
+     *
+     */
+    void lock()
+    {
+        if (!m_locked)
+        {
+            m_mutex.lock();
+            m_locked = true;
+        }
+    }
+
+    /**
+     * @brief 解锁
+     *
+     */
+    void unlock()
+    {
+        if (m_locked)
+        {
+            m_mutex.unlock();
+            m_locked = false;
+        }
+    }
+
+};
 
 
+
+
+/**
+ * @brief 自旋锁
+ *
+//  */
+// class Spinlock : Noncopyable{
+//     public:
+//         // 局部锁
+//         // typedef Scoped
+// }
 
 
 
